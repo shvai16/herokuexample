e110ba97ed0b0b346f153d4f59061bf872a2af52
shvai16 <47330041+shvai16@users.noreply.github.com>
2020-12-14 15:54:39 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Addition
+{    
+	
+	public void add(int f,int s)
+	{
+   int  result = f + s;
+    System.out.println("Addition="+result);
+	System.out.println("=============================================");
+	}
+
+}
